3d0f2b87e024cc2aa3298e9a0c7d125a0130ed92
Eddie Padin <eddie@entertainermedia.net>
2018-10-18 13:47:50 -0500

    - refactored MovieProfile class component to SFC;

--- src/components/MovieProfile.jsx
+++ src/components/MovieProfile.jsx
@@ -1,53 +1,49 @@
-import React, { Component } from "react";
+import React from "react";
 import UserList from "./UserList";
 
-class MovieProfile extends Component {
-  render() {
-    //Destructure the props
-    const { users, usersWhoLikedMovie, movieInfo } = this.props;
-
-    return (
-      <div className="col-12 col-lg-4" key={movieInfo.id}>
-        <div className="flip-box">
-          <div className="flip flip_horizontal flip-box__multiply">
-            <div className="flip__front d-flex flex-column justify-content-center">
-              <div className="flip-box__multiply-title  font-weight-bold text-center text text-uppercase pl-lg-5 pr-lg-5 pl-3 pr-3">
-                <h2>{movieInfo.name}</h2>
-              </div>
-              <div
-                className="flip-box__multiply-decoration mx-auto mb-3"
-                style={{
-                  backgroundImage: `url(/images/movies/movie_${
-                    movieInfo.id
-                  }.jpg)`,
-                  backgroundSize: "cover"
-                }}
-              />
-              <div className="row">
-                <div className="col col-12">
-                  <h4 className="text-center">Liked By: </h4>
-                </div>
-              </div>
-              <div className="row justify-content-center">
-                <div className="col col-8">
-                  <UserList
-                    usersWhoLikedMovie={usersWhoLikedMovie}
-                    users={users}
-                  />
-                </div>
-              </div>
+//ES6 function with implicit return
+const MovieProfile = props => (
+  <div className="col-12 col-lg-4" key={props.movieInfo.id}>
+    <div className="flip-box">
+      <div className="flip flip_horizontal flip-box__multiply">
+        <div className="flip__front d-flex flex-column justify-content-center">
+          <div className="flip-box__multiply-title  font-weight-bold text-center text text-uppercase pl-lg-5 pr-lg-5 pl-3 pr-3">
+            <h2>{props.movieInfo.name}</h2>
+          </div>
+          <div
+            className="flip-box__multiply-decoration mx-auto mb-3"
+            style={{
+              backgroundImage: `url(/images/movies/movie_${
+                props.movieInfo.id
+              }.jpg)`,
+              backgroundSize: "cover"
+            }}
+          />
+          <div className="row">
+            <div className="col col-12">
+              <h4 className="text-center">Liked By: </h4>
             </div>
-            <div
-              className="flip__back  flip-box__bg flip-box__bg_overlay d-flex flex-column justify-content-center"
-              style={{
-                backgroundImage: `url(/images/movies/movie_${movieInfo.id}.jpg)`
-              }}
-            />
+          </div>
+          <div className="row justify-content-center">
+            <div className="col col-8">
+              <UserList
+                usersWhoLikedMovie={props.usersWhoLikedMovie}
+                users={props.users}
+              />
+            </div>
           </div>
         </div>
+        <div
+          className="flip__back  flip-box__bg flip-box__bg_overlay d-flex flex-column justify-content-center"
+          style={{
+            backgroundImage: `url(/images/movies/movie_${
+              props.movieInfo.id
+            }.jpg)`
+          }}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default MovieProfile;
