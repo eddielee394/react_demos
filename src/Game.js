0a57ab6d9b80a53d30abe1ae98c2cf780fdad982
Eddie Padin <eddie@entertainermedia.net>
2018-10-19 01:03:13 -0500

    - fixed button name for true submit; - added answer logic;

--- src/Game.js
+++ src/Game.js
@@ -3,19 +3,39 @@ import Equation from "./components/game/Equation";
 import AnswerSubmit from "./components/game/AnswerSubmit";
 import Score from "./components/game/Score";
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-
 class Game extends Component {
-    state = {
-        numCorrect  : 0,
-        numQuestions: 2
+    constructor(props) {
+        super(props);
+        const valuesArray = this.makeNewQuestion();
+        this.state = {
+            numCorrect  : 0,
+            numQuestions: 2,
+            value1: valuesArray[0],
+            value2: valuesArray[1],
+            value3: valuesArray[2],
+            proposedAnswer: valuesArray[3],
+        }
     };
     
-    //Answer logic
-    handleAnswer = answerWasCorrect => {
+    makeNewQuestion = () => {
+        const value1 = Math.floor(Math.random() * 100);
+        const value2 = Math.floor(Math.random() * 100);
+        const value3 = Math.floor(Math.random() * 100);
+        const proposedAnswer = Math.floor(Math.random() * 3) + (value1 + value2 + value3);
+        return [value1, value2, value3, proposedAnswer];
+    };
+    
+    updateState = newValuesArray => {
+        this.setState(currState => ({
+            value1: newValuesArray[0],
+            value2: newValuesArray[1],
+            value3: newValuesArray[2],
+            proposedAnswer: newValuesArray[3],
+        }));
+    };
+    
+    //Handle the Score logic
+    handleScore = answerWasCorrect => {
         //first we check if the answer was correct
         if (answerWasCorrect) {
             //then we set the state of the correctAnswer prop to increment by 1
@@ -29,14 +49,32 @@ class Game extends Component {
         }));
     };
     
+    handleAnswer = event => {
+        const newValuesArray = this.makeNewQuestion();
+        this.updateState(newValuesArray);
+        const answerWasCorrect = this.evaluateAnswer(event.target.name);
+        this.handleScore(answerWasCorrect);
+    };
+    
+    evaluateAnswer(givenAnswer) {
+        const { value1, value2, value3, proposedAnswer } = this.state;
+        const corrAnswer = value1 + value2 + value3;
+        
+        return (
+            (corrAnswer === proposedAnswer && givenAnswer === 'true') ||
+            (corrAnswer !== proposedAnswer && givenAnswer === 'false')
+        );
+    }
+    
     render() {
+        const { value1, value2, value3, proposedAnswer } = this.state;
         return (
             //next we add the equation component and values
             <div className="game">
                 <h2>Mental Math</h2>
                 <Equation value1={value1} value2={value2} value3={value3} proposedAnswer={proposedAnswer} />
-                <AnswerSubmit handleAnswer={this.handleAnswer}/>
-               <Score numCorrect={this.state.numCorrect} numQuestions={this.state.numQuestions} />
+                <AnswerSubmit handleAnswer={this.handleAnswer} />
+                <Score numCorrect={this.state.numCorrect} numQuestions={this.state.numQuestions} />
             </div>
         );
     }
--- src/components/game/AnswerSubmit.jsx
+++ src/components/game/AnswerSubmit.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AnswerSubmit = props => {
   return (
     <div className="btn-group">
-      <button onClick={props.handleAnswer} name="false">
+      <button onClick={props.handleAnswer} name="true">
         True
       </button>
       <button onClick={props.handleAnswer} name="false">
